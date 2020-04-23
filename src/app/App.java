561203dfa57e9901d8b42474e071ed2ec08c88a8
Leo <leonardo.antazu@upeu.edu.pe>
2020-04-22 22:30:48 -0500

    change1

--- src/app/App.java
+++ src/app/App.java
@@ -7,55 +7,55 @@ public class App {
     public static Scanner t = new Scanner(System.in);
 
     public static void menu(){
-        System.out.println("====  MENU   ====");
+        System.out.println("-----  MENU  -----");
         System.out.println("1. SUMA          ");
         System.out.println("2. RESTA         ");
         System.out.println("3. MULTIPLICACION");
         System.out.println("4. DIVISION      ");
         System.out.println("5. Salir         ");
-        System.out.print("- Opcion: ");
+        System.out.print("Opcion: ");
     }
     public static void suma(){
-        int num1, num2, res;
-        System.out.print("Ingrese el primer numero: ");
-        num1 = Integer.valueOf(t.nextLine());
-        System.out.print("Ingrese el segundo numero: ");
-        num2 = Integer.valueOf(t.nextLine());
+        double num1, num2, res;
+        System.out.print("Digite el primer numero: ");
+        num1 = Double.valueOf(t.nextLine());
+        System.out.print("Digite el segundo numero: ");
+        num2 = Double.valueOf(t.nextLine());
         res = num1 + num2;
         System.out.println("Respuesta: "+ res);
     }
     public static void resta(){
-        int num1, num2, res;
-        System.out.print("Ingrese el primer numero: ");
-        num1 = Integer.valueOf(t.nextLine());
-        System.out.print("Ingrese el segundo numero: ");
-        num2 = Integer.valueOf(t.nextLine());
+        double num1, num2, res;
+        System.out.print("Digite el primer numero: ");
+        num1 = Double.valueOf(t.nextLine());
+        System.out.print("Digite el segundo numero: ");
+        num2 = Double.valueOf(t.nextLine());
         res = num1 - num2;
         System.out.println("Respuesta: "+ res);
     }
     public static void multiplicacion(){
-        int num1, num2, res;
-        System.out.print("Ingrese el primer numero: ");
-        num1 = Integer.valueOf(t.nextLine());
-        System.out.print("Ingrese el segundo numero: ");
-        num2 = Integer.valueOf(t.nextLine());
+        double num1, num2, res;
+        System.out.print("Digite el primer numero: ");
+        num1 = Double.valueOf(t.nextLine());
+        System.out.print("Digite el segundo numero: ");
+        num2 = Double.valueOf(t.nextLine());
         res = num1 * num2;
         System.out.println("Respuesta: "+ res);
     }
     public static void division(){
-        int num1, num2, res;
-        System.out.print("Ingrese el primer numero: ");
-        num1 = Integer.valueOf(t.nextLine());
-        System.out.print("Ingrese el segundo numero: ");
-        num2 = Integer.valueOf(t.nextLine());
+        double num1, num2, res;
+        System.out.print("Digite el primer numero: ");
+        num1 = Double.valueOf(t.nextLine());
+        System.out.print("Digite el segundo numero: ");
+        num2 = Double.valueOf(t.nextLine());
         res = num1 / num2;
         System.out.println("Respuesta: "+ res);
     }
     public static void salir(){
-        System.out.println("Gracias por usar el programa");
+        System.out.println("Programa finalizado!");
     }
     public static void error(){
-        System.out.println("Ingrese una opcion entre 1 a 5...");
+        System.out.println("Fuera de Rango...");
     }
     public static void inicio(){
         int op;
